75e55e492df6cdb68859a5efa9a2cf0caaf7a744
vdvman1 <vdvman88@gmail.com>
2020-12-29 22:24:42 +1100

    feat: load WASM binary into memory

--- WASM-To-MC/Program.cs
+++ WASM-To-MC/Program.cs
@@ -7,3 +7,20 @@ if(Arguments.Parse(args) is not Arguments arguments)
 {
     return;
 }
+
+byte[] bytes;
+try
+{
+    bytes = await File.ReadAllBytesAsync(arguments.Input);
+}
+catch (IOException e) when (e is DirectoryNotFoundException or FileNotFoundException)
+{
+    Console.Error.WriteLine($"Input WASM binary could not be found: {arguments.Input}");
+    return;
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine($"Unable to read input WASM binary: {arguments.Input}");
+    Console.WriteLine(e);
+    return;
+}
